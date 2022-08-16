33ee7101445230c2712049be69c740399378b047
kamausuzan197 <kamaususan197@gmail.com>
2022-08-16 12:40:51 +0300

    complete

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+     "Hello, my name is #{name}."
+end 
+ 
+badge_maker ("Arel")
+
+def batch_badge_creator(array)
+    array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+    room = 0
+    rooms_array = array.map do |name|
+        room += 1
+        "Hello, #{name}! You'll be assigned to room #{room}!" 
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each {|p| puts p}
+    assign_rooms(array).each {|p| puts p}
+end
